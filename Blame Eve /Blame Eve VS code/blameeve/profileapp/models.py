2b1e08fa0ab9ea233466b9805a579e9928b81422
kaceoria <50686400+kaceoria@users.noreply.github.com>
2023-12-10 23:30:46 -0600

    Add files via upload
    more files

---
+++ Blame Eve /Blame Eve VS code/blameeve/profileapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+User = get_user_model()
+
+class Profile(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.TextField()
+    last_name = models.TextField()
+    location = models.CharField(max_length=100, blank = True)
+    age = models.IntegerField()
+    bio = models.TextField(blank = True)
+
+def __str__(self):
+    return self.user.username
